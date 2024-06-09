57264b4687d0de5f15153fb417829e5be870b40f
jkbk2004 <jong.kim@noaa.gov>
2024-06-09 17:28:20 -0500

    a

---
+++ modulefiles.hercules/del.conf/modules.nems.sh
@@ -0,0 +1,3 @@
+. /work/noaa/da/ycteng/work8/godas/src/DATM-MOM6-CICE5.fd/NEMS/src/conf/module-setup.sh.inc
+module use /work/noaa/da/ycteng/work8/godas/src/DATM-MOM6-CICE5.fd/NEMS/src/conf
+module load modules.nems
